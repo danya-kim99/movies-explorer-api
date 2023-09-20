46c1f35881bc42651e2435894abde8910470a37e
Daniil <visomnibus@yandex.ru>
2023-09-20 21:48:16 +0300

    schemas and infra

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const UnauthorizedError = require('../errors/unauthorized-err');
+
+const regex = require('../utils/regex');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Поле "email" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Некорректный email',
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Поле "password" должно быть заполнено'],
+      select: false,
+    },
+    name: {
+      type: String,
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+      default: 'Даниил',
+    },
+  },
+  { versionKey: false },
+);
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
